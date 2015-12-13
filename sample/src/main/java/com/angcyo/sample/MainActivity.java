add7f5fb1b4ec5ad50c2edccb7c336db3254dc1e
angcyo <angcyo@126.com>
2015-12-13 14:22:38 +0800

    删除 app module

--- sample/src/main/java/com/angcyo/sample/MainActivity.java
+++ sample/src/main/java/com/angcyo/sample/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.angcyo.rjcenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
