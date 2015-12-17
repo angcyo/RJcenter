3df1a3eead15e4fdda8a93dfd9f5ae3b0d613c7a
angcyo <angcyo@126.com>
2015-12-17 16:00:32 +0800

    新增Toast工具类, 全局只有一个Toast(可以更新之前的toast)

--- rxjavademo/src/main/java/com/angcyo/rxjavademo/MainActivity.java
+++ rxjavademo/src/main/java/com/angcyo/rxjavademo/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.rsen.util.T;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        T.show(MainActivity.this, "onCreate");
     }
 
     @Override
