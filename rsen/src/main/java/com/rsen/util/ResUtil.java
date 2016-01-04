b2ba90873a61cfe265dfabdd332f340a8ac19120
angcyo <angcyo@126.com>
2016-01-04 19:40:47 +0800

    view setRotation setRotationX setRotationY test

---
+++ rsen/src/main/java/com/rsen/util/ResUtil.java
@@ -0,0 +1,90 @@
+package com.rsen.util;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
+import android.os.Build;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * Created by angcyo on 15-12-31 031 10:52 上午.
+ */
+public class ResUtil {
+    /**
+     * Px to dp float.
+     *
+     * @param res the res
+     * @param px  the px
+     * @return the float
+     */
+    public static float pxToDp(Resources res, float px) {
+        float dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, res.getDisplayMetrics());
+
+        return dp;
+    }
+
+    /**
+     * Generate text color color state list.
+     *
+     * @param pressColor   the press color
+     * @param defaultColor the default color
+     * @return the color state list
+     */
+    public static ColorStateList generateTextColor(int pressColor, int defaultColor) {
+        ColorStateList stateList = new ColorStateList(new int[][]{{android.R.attr.state_pressed}, {}}, new int[]{pressColor, defaultColor});
+        return stateList;
+    }
+
+    /**
+     * Generate bg drawable drawable.
+     *
+     * @param radii        the radii
+     * @param borderWidth  the border width
+     * @param pressColor   the press color
+     * @param defaultColor the default color
+     * @return the drawable
+     */
+    public static Drawable generateBgDrawable(Resources res, float radii, float borderWidth, int pressColor, int defaultColor) {
+
+        radii = pxToDp(res, radii);
+        borderWidth = pxToDp(res, borderWidth);
+
+        //外环的圆角矩形
+        float[] outRadii = new float[]{radii, radii, radii, radii, radii, radii, radii, radii};//四个角的 圆角幅度,8个可以设置的值,每个角都有2个边 2*4=8个
+
+        //与内环的距离
+        RectF inset = new RectF(borderWidth, borderWidth, borderWidth, borderWidth);
+
+        //按下状态
+        Shape roundRectShape = new RoundRectShape(outRadii, inset, null);//圆角背景
+        ShapeDrawable shopDrawablePress = new ShapeDrawable(roundRectShape);//圆角shape
+        shopDrawablePress.getPaint().setColor(pressColor);//设置颜色
+
+        //正常状态
+        Shape roundRectShapeNormal = new RoundRectShape(outRadii, inset, null);
+        ShapeDrawable shopDrawableNormal = new ShapeDrawable(roundRectShapeNormal);
+        shopDrawableNormal.getPaint().setColor(defaultColor);
+
+        StateListDrawable bgStateDrawable = new StateListDrawable();//状态shape
+        bgStateDrawable.addState(new int[]{android.R.attr.state_pressed}, shopDrawablePress);//按下状态
+        bgStateDrawable.addState(new int[]{}, shopDrawableNormal);//其他状态
+
+        return bgStateDrawable;
+    }
+
+    @SuppressLint("NewApi")
+    public static void setBgDrawable(View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackground(drawable);
+        } else {
+            view.setBackgroundDrawable(drawable);
+        }
+    }
+}
--- sample/src/main/java/com/angcyo/sample/ViewRotationActivity.java
+++ sample/src/main/java/com/angcyo/sample/ViewRotationActivity.java
@@ -3,17 +3,19 @@ package com.angcyo.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.cocosw.query.CocoQuery;
+import com.rsen.util.ResUtil;
 
 public class ViewRotationActivity extends AppCompatActivity {
 
     CocoQuery q;
     private View target;
-    private TextView xV, yV;
-    private float x, y, rotation = 20f;
+    private TextView xV, yV, rotate;
+    private float x, y, rotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,9 @@ public class ViewRotationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_rotation);
         q = new CocoQuery(this);
         target = q.id(R.id.target).getView();
+        rotate = (TextView) q.id(R.id.rotate).getView();
         xV = (TextView) q.id(R.id.x).getView();
-        yV = (TextView) q.id(R.id.x).getView();
+        yV = (TextView) q.id(R.id.y).getView();
 
     }
 
@@ -50,15 +53,35 @@ public class ViewRotationActivity extends AppCompatActivity {
     private void init() {
         String xs = xV.getText().toString();
         String ys = yV.getText().toString();
+        String rS = rotate.getText().toString();
         if (TextUtils.isEmpty(xs)) {
             x = target.getWidth() / 2f;
         } else {
-            x = Float.valueOf(xs);
+            x = dp(Float.valueOf(xs));
         }
         if (TextUtils.isEmpty(ys)) {
             y = target.getHeight() / 2f;
         } else {
-            y = Float.valueOf(ys);
+            y = dp(Float.valueOf(ys));
         }
+        if (TextUtils.isEmpty(rS)) {
+            rotation = 20f;
+        } else {
+            rotation = Float.valueOf(rS);
+        }
+
+        target.setPivotX(x);
+        target.setPivotY(y);
+
+        e("w:" + target.getWidth() + " h:" + target.getHeight());
+        e("x:" + x + " y:" + y + " r:" + rotation);
+    }
+
+    private float dp(float v) {
+        return ResUtil.pxToDp(getResources(), v);
+    }
+
+    private void e(String msg) {
+        Log.e("angcyo", msg + "");
     }
 }
