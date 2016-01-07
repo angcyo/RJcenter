178291025a51c176d2126bd926159707471ee375
angcyo <angcyo@126.com>
2016-01-07 17:54:33 +0800

    add tag radio group layout view

--- rsen/src/main/java/com/rsen/view/viewgroup/TagRadioGroup.java
+++ rsen/src/main/java/com/rsen/view/viewgroup/TagRadioGroup.java
@@ -2,6 +2,7 @@ package com.rsen.view.viewgroup;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.RectF;
 import android.graphics.drawable.ColorDrawable;
@@ -10,31 +11,40 @@ import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.graphics.drawable.shapes.Shape;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.angcyo.rsen.R;
 import com.rsen.util.ResUtil;
 
 /**
  * 标签单选按钮组
  * Created by angcyo on 2016-01-06.
  */
-public class TagRadioGroup extends RadioGroup {
+public class TagRadioGroup extends RadioGroup implements RadioGroup.OnCheckedChangeListener {
+    public static final String KEY_CHECK = "key_check";
     private int childWidth = ViewGroup.LayoutParams.WRAP_CONTENT;
     private int childHeight = ViewGroup.LayoutParams.WRAP_CONTENT;
     private int childTextColor = Color.parseColor("#969696");
     private int childPressTextColor = Color.WHITE;
+    private int padding = -1;//dp
     private int paddingLeft = 15;//dp
     private int paddingTop = 10;
     private int paddingRight = 15;
     private int paddingBottom = 10;
-
     private float borderWidth = 4f;//线框的宽度
-
     private float round = 6f;//dp 单位
+    private int childCount = -1;//自动填充子view的数量
+    private String[] childTexts;//填充子view的文本
+    private int checkButtonIndex = 0;//默认选中按钮
+    private int curCheckButtonIndex = -1;
+    private String curCheckButtonText;
+    private OnCheckListener listener;
 
     public TagRadioGroup(Context context) {
         this(context, null);
@@ -42,6 +52,23 @@ public class TagRadioGroup extends RadioGroup {
 
     public TagRadioGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TagRadioGroup);
+        childWidth = (int) typedArray.getDimension(R.styleable.TagRadioGroup_childWidth, childWidth);
+        childHeight = (int) typedArray.getDimension(R.styleable.TagRadioGroup_childHeight, childHeight);
+        padding = typedArray.getInteger(R.styleable.TagRadioGroup_childPadding, padding);
+        paddingLeft = typedArray.getInteger(R.styleable.TagRadioGroup_childPaddingLeft, paddingLeft);
+        paddingTop = typedArray.getInteger(R.styleable.TagRadioGroup_childPaddingTop, paddingTop);
+        paddingRight = typedArray.getInteger(R.styleable.TagRadioGroup_childPaddingRight, paddingRight);
+        paddingBottom = typedArray.getInteger(R.styleable.TagRadioGroup_childPaddingBottom, paddingBottom);
+
+        childTextColor = typedArray.getColor(R.styleable.TagRadioGroup_childTextColor, childTextColor);
+        childPressTextColor = typedArray.getColor(R.styleable.TagRadioGroup_childTextColorPress, childPressTextColor);
+
+        borderWidth = typedArray.getDimension(R.styleable.TagRadioGroup_childBorderWidth, borderWidth);
+        round = typedArray.getDimension(R.styleable.TagRadioGroup_childRound, round);
+
+        childCount = typedArray.getInteger(R.styleable.TagRadioGroup_childCount, childCount);
+        checkButtonIndex = typedArray.getInteger(R.styleable.TagRadioGroup_checkButtonIndex, checkButtonIndex);
         init();
     }
 
@@ -77,14 +104,59 @@ public class TagRadioGroup extends RadioGroup {
     }
 
     private void init() {
+        fillChildButton();
+        setOnCheckedChangeListener(this);
+    }
 
+    @Override
+    public void addView(View child) {
+        if (childCount <= 0) {
+            super.addView(child);
+        }
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        if (childCount <= 0) {
+            super.addView(child, index);
+        }
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        if (childCount <= 0) {
+            super.addView(child, width, height);
+        }
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        if (childCount <= 0) {
+            super.addView(child, params);
+        }
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+    }
+
+    private RadioButton generateChildButton(int index) {
+        RadioButton button = new RadioButton(getContext(), null, android.R.attr.radioButtonStyle);
+        String text = "";
+        if (childTexts != null && childTexts.length > index) {
+            text = childTexts[index];
+        }
+        button.setText(text);
+        return button;
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
-        initChildButton();
+        if (childCount <= 0) {
+            initChildButton();
+        }
     }
 
     private void initChildButton() {
@@ -95,7 +167,11 @@ public class TagRadioGroup extends RadioGroup {
                 RadioButton childView = (RadioButton) view;
                 childView.setButtonDrawable(new ColorDrawable());//去掉系统默认的圆圈
                 childView.setTextColor(textColor());
-                childView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+                if (padding <= 0) {
+                    childView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+                } else {
+                    childView.setPadding(padding, padding, padding, padding);
+                }
 
                 if (i == 0) {
                     initFirstButton(childView);
@@ -126,5 +202,193 @@ public class TagRadioGroup extends RadioGroup {
 
     private void initFirstButton(RadioButton childView) {
         ResUtil.setBgDrawable(childView, bgDrawable(round, 0, new RectF(borderWidth, borderWidth, borderWidth / 2, borderWidth)));
+    }
+
+    private void fillChildButton() {
+        removeAllViews();
+        for (int i = 0; i < childCount; i++) {
+            addView(generateChildButton(i), i, new ViewGroup.LayoutParams(childWidth, childHeight));
+        }
+        initChildButton();
+
+        if (checkButtonIndex > 0) {
+            RadioButton button = (RadioButton) getChildAt(checkButtonIndex);
+            if (button != null) {
+                button.setChecked(true);
+            }
+        }
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_CHECK, getCheckIndex());
+        bundle.putParcelable("state", super.onSaveInstanceState());
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        Bundle bundle = (Bundle) state;
+        int index = bundle.getInt(KEY_CHECK);
+        RadioButton childAt = (RadioButton) getChildAt(index);
+        if (childAt != null) {
+            childAt.setChecked(true);
+        }
+        super.onRestoreInstanceState(bundle.getParcelable("state"));
+    }
+
+    private void checkView() {
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            RadioButton button = (RadioButton) getChildAt(i);
+            if (button.isChecked()) {
+                curCheckButtonIndex = i;
+                curCheckButtonText = button.getText().toString();
+            }
+        }
+    }
+
+    /**
+     * 获取选中按钮的索引
+     */
+    public int getCheckIndex() {
+        checkView();
+        return curCheckButtonIndex;
+    }
+
+    /**
+     * 获取选中按钮的文本
+     */
+    public String getCheckText() {
+        checkView();
+        return curCheckButtonText;
+    }
+
+    public int getChildWidth() {
+        return childWidth;
+    }
+
+    public void setChildWidth(int childWidth) {
+        this.childWidth = childWidth;
+    }
+
+    public int getChildHeight() {
+        return childHeight;
+    }
+
+    public void setChildHeight(int childHeight) {
+        this.childHeight = childHeight;
+    }
+
+    public int getChildTextColor() {
+        return childTextColor;
+    }
+
+    public void setChildTextColor(int childTextColor) {
+        this.childTextColor = childTextColor;
+    }
+
+    public int getChildPressTextColor() {
+        return childPressTextColor;
+    }
+
+    public void setChildPressTextColor(int childPressTextColor) {
+        this.childPressTextColor = childPressTextColor;
+    }
+
+    public int getPadding() {
+        return padding;
+    }
+
+    public void setPadding(int padding) {
+        this.padding = padding;
+    }
+
+    @Override
+    public int getPaddingLeft() {
+        return paddingLeft;
+    }
+
+    public void setPaddingLeft(int paddingLeft) {
+        this.paddingLeft = paddingLeft;
+    }
+
+    @Override
+    public int getPaddingTop() {
+        return paddingTop;
+    }
+
+    public void setPaddingTop(int paddingTop) {
+        this.paddingTop = paddingTop;
+    }
+
+    @Override
+    public int getPaddingRight() {
+        return paddingRight;
+    }
+
+    public void setPaddingRight(int paddingRight) {
+        this.paddingRight = paddingRight;
+    }
+
+    @Override
+    public int getPaddingBottom() {
+        return paddingBottom;
+    }
+
+    public void setPaddingBottom(int paddingBottom) {
+        this.paddingBottom = paddingBottom;
+    }
+
+    public float getBorderWidth() {
+        return borderWidth;
+    }
+
+    public void setBorderWidth(float borderWidth) {
+        this.borderWidth = borderWidth;
+    }
+
+    public float getRound() {
+        return round;
+    }
+
+    public void setRound(float round) {
+        this.round = round;
+    }
+
+    @Override
+    public int getChildCount() {
+        return childCount;
+    }
+
+    public void setChildCount(int childCount) {
+        this.childCount = childCount;
+        fillChildButton();
+    }
+
+    public String[] getChildTexts() {
+        return childTexts;
+    }
+
+    public void setChildTexts(String[] childTexts) {
+        this.childTexts = childTexts;
+        fillChildButton();
+    }
+
+    public void setListener(OnCheckListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        checkView();
+        if (listener != null) {
+            listener.onCheck(this, (RadioButton) getChildAt(curCheckButtonIndex), curCheckButtonIndex, curCheckButtonText);
+        }
+    }
+
+    public interface OnCheckListener {
+        void onCheck(TagRadioGroup radioGroup, RadioButton button, int index, String text);
     }
 }
--- sample/src/main/java/com/angcyo/sample/viewdemo/TagRadioGroupActivity.java
+++ sample/src/main/java/com/angcyo/sample/viewdemo/TagRadioGroupActivity.java
@@ -2,14 +2,31 @@ package com.angcyo.sample.viewdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
 
 import com.angcyo.sample.R;
+import com.rsen.util.T;
+import com.rsen.view.viewgroup.TagRadioGroup;
 
 public class TagRadioGroupActivity extends AppCompatActivity {
+    TagRadioGroup tagGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_radio_group);
+
+        tagGroup = (TagRadioGroup) findViewById(R.id.tag_group);
+
+        tagGroup.setChildCount(6);
+        tagGroup.setBorderWidth(3f);
+        tagGroup.setChildTexts(new String[]{"消息", "新闻", "交友", "未读", "提醒"});
+
+        tagGroup.setListener(new TagRadioGroup.OnCheckListener() {
+            @Override
+            public void onCheck(TagRadioGroup radioGroup, RadioButton button, int index, String text) {
+                T.show(TagRadioGroupActivity.this, text + "--" + index + "---" + button.getId());
+            }
+        });
     }
 }
