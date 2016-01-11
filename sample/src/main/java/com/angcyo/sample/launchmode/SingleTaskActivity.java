61653a7c2ac2b6c5d69eba9bdd05398abef69107
angcyo <angcyo@126.com>
2016-01-11 23:46:43 +0800

    add single task launch mode sample

---
+++ sample/src/main/java/com/angcyo/sample/launchmode/SingleTaskActivity.java
@@ -0,0 +1,36 @@
+package com.angcyo.sample.launchmode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.angcyo.sample.R;
+
+public class SingleTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void button2(View view) {
+        startActivity(new Intent(this, SingleActivity2.class));
+    }
+
+}
