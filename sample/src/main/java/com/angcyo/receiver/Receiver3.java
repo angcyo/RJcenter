10aecfa7f96871b1a8e9f6d84a9c4335de8d36ab
angcyo <angcyo@126.com>
2016-01-14 01:12:12 +0800

    add package manager sample

---
+++ sample/src/main/java/com/angcyo/receiver/Receiver3.java
@@ -0,0 +1,15 @@
+package com.angcyo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by angcyo on 16-01-14-014.
+ */
+public class Receiver3 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ sample/src/main/java/com/angcyo/sample/pmdemo/PackageManagerActivity.java
@@ -0,0 +1,101 @@
+package com.angcyo.sample.pmdemo;
+
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.angcyo.sample.R;
+
+public class PackageManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_manager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initView();
+    }
+
+    TextView textView;
+    private void initView() {
+        textView = (TextView) findViewById(R.id.text);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(getAllActivitys());
+        builder.append(getAllServices());
+        builder.append(getAllReceivers());
+        textView.setText(builder.toString());
+    }
+
+    private PackageInfo getPackInfo(int flag) {
+        try {
+            return getPackageManager().getPackageInfo(getPackageName(), flag);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getAllActivitys() {
+        StringBuilder builder = new StringBuilder();
+        line(builder);
+        builder.append("----------所有的Activity");
+        ActivityInfo[] activities = getPackInfo(PackageManager.GET_ACTIVITIES).activities;
+        for (ActivityInfo info : activities) {
+            line(builder);
+            builder.append(info.targetActivity);
+        }
+
+        return builder.toString();
+    }
+
+    private String getAllServices() {
+        StringBuilder builder = new StringBuilder();
+        line(builder);
+        builder.append("----------所有的Services");
+        ServiceInfo[] services = getPackInfo(PackageManager.GET_SERVICES).services;
+        for (ServiceInfo info : services) {
+            line(builder);
+            builder.append(info.getClass().getName());
+        }
+
+        return builder.toString();
+    }
+
+    private String getAllReceivers() {
+        StringBuilder builder = new StringBuilder();
+
+        line(builder);
+        builder.append("----------所有的Receivers");
+        ActivityInfo[] receivers = getPackInfo(PackageManager.GET_RECEIVERS).receivers;
+        for (ActivityInfo info : receivers) {
+            line(builder);
+            builder.append(info.toString());
+        }
+
+        return builder.toString();
+    }
+
+    private void line(StringBuilder builder) {
+        builder.append("\n");
+    }
+}
