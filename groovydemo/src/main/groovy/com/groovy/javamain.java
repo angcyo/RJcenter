6c44d6cffe57d9d68e344358726dccf09d694e3e
angcyo <angcyo@126.com>
2016-01-13 23:41:55 +0800

    add groovy sample

--- groovydemo/src/main/groovy/com/groovy/javamain.java
+++
@@ -1,9 +0,0 @@
-package com.groovy;
-
-public class javamain {
-    public static final int main(String... msg) {
-        for (String s : msg)
-            System.out.println(s);
-        return -1;
-    }
-}
