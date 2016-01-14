982fcaa81eba2a3c47bbf22cd787d5f3e7182f96
angcyo <angcyo@126.com>
2016-01-14 14:08:02 +0800

    add span string sample

--- sample/src/main/java/com/angcyo/sample/spandemo/SpanDemoActivity.java
+++ sample/src/main/java/com/angcyo/sample/spandemo/SpanDemoActivity.java
@@ -30,6 +30,8 @@ public class SpanDemoActivity extends AppCompatActivity {
 
     static void e(String log) {
         Log.e("angcyo", log);
+        Log.e("angcyo", log);
+        Log.e("angcyo", log);
     }
 
     @Override
