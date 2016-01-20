4e23aa327d33a2f31877036307ea4bc0d839b0e6
angcyo <angcyo@126.com>
2016-01-20 23:54:19 +0800

    add shape background sample

--- sample/src/main/java/com/angcyo/sample/ShapeDemo/ShapeActivity.java
+++ sample/src/main/java/com/angcyo/sample/ShapeDemo/ShapeActivity.java
@@ -1,8 +1,8 @@
 package com.angcyo.sample.ShapeDemo;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,10 +22,15 @@ public class ShapeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                TransitionDrawable transitionDrawable = (TransitionDrawable) findViewById(R.id.edit_text).getBackground();
+                transitionDrawable.startTransition(1000);
             }
         });
+    }
+
+    public void button(View view) {
+        finish();
+        overridePendingTransition(com.angcyo.rsen.R.anim.scale_0to1, com.angcyo.rsen.R.anim.scale_1to0);
     }
 
 }
