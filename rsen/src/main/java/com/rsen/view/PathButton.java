fc50c2c29935fa9d68e333ecab514aacdad53c53
angcyo <angcyo@126.com>
2016-01-24 14:06:45 +0800

    fix bug

--- rsen/src/main/java/com/rsen/view/PathButton.java
+++ rsen/src/main/java/com/rsen/view/PathButton.java
@@ -110,7 +110,7 @@ public class PathButton extends Button {
     }
 
     public PathButton(Context context, AttributeSet attrs) {
-        this(context, attrs, android.R.attr.buttonStyle);
+        this(context, attrs, android.support.v7.appcompat.R.attr.buttonStyle);
     }
 
     public PathButton(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -202,7 +202,7 @@ public class PathButton extends Button {
 
 //        mCurDrawDelay = 1000 / (mViewWidth + mViewHeight) / mCurDrawStep;
 
-        mCurDrawStep = Math.min(mViewHeight, mViewWidth) / mCurDrawDelay ;
+        mCurDrawStep = Math.min(mViewHeight, mViewWidth) / mCurDrawDelay;
 
 
         if (background != null) {
