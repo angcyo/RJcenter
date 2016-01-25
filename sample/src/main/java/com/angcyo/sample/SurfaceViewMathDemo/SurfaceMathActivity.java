b7e17f3c18cfd93240036d9bbcc3099ea8d256b0
angcyo <angcyo@126.com>
2016-01-25 13:43:51 +0800

    add sin cos math demo

--- sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/SurfaceMathActivity.java
+++ sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/SurfaceMathActivity.java
@@ -23,7 +23,7 @@ public class SurfaceMathActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null).show();//
             }
         });
     }
