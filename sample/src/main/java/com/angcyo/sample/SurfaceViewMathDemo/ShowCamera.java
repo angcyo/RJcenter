87f6add86655f373ddfb871986112e87a7c974a1
angcyo <angcyo@126.com>
2016-03-16 14:51:48 +0800

    Camera Preview DisplayOrientation

--- sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/ShowCamera.java
+++ sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/ShowCamera.java
@@ -5,18 +5,25 @@ package com.angcyo.sample.SurfaceViewMathDemo;
  */
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
+
+import java.util.List;
 
 
 /**
  * 预览摄像头
  */
+@SuppressWarnings("deprecation")
 public class ShowCamera extends SurfaceView {
 
+    public static final String TAG = "ShowCamera";
     public SurfaceHolder surfaceHolder;
     int rotate;
     private Camera mCamera;
@@ -55,11 +62,65 @@ public class ShowCamera extends SurfaceView {
                 try {
                     /**从新开启预览*/
                     Camera.Parameters parameters = mCamera.getParameters();
+                    try {
+                        // parameters.setFlashMode("off"); // 无闪光灯
+                        parameters.setPictureFormat(PixelFormat.JPEG); //Sets the image format for picture 设定相片格式为JPEG，默认为NV21
+                        parameters.setPreviewFormat(PixelFormat.YCbCr_420_SP); //Sets the image format for preview picture，默认为NV21
+                        /*【ImageFormat】JPEG/NV16(YCrCb format，used for Video)/NV21(YCrCb format，used for Image)/RGB_565/YUY2/YU12*/
+
+                        // 【调试】获取caera支持的PictrueSize，看看能否设置？？
+                        List<Camera.Size> pictureSizes = mCamera.getParameters().getSupportedPictureSizes();
+                        List<Camera.Size> previewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+                        List<Integer> previewFormats = mCamera.getParameters().getSupportedPreviewFormats();
+                        List<Integer> previewFrameRates = mCamera.getParameters().getSupportedPreviewFrameRates();
+                        Log.i(TAG + "initCamera", "cyy support parameters is ");
+                        Camera.Size psize = null;
+                        for (int i = 0; i < pictureSizes.size(); i++) {
+                            psize = pictureSizes.get(i);
+                            Log.i(TAG + "initCamera", "PictrueSize,width: " + psize.width + " height" + psize.height);
+                        }
+                        for (int i = 0; i < previewSizes.size(); i++) {
+                            psize = previewSizes.get(i);
+                            Log.i(TAG + "initCamera", "PreviewSize,width: " + psize.width + " height" + psize.height);
+                        }
+                        Integer pf = null;
+                        for (int i = 0; i < previewFormats.size(); i++) {
+                            pf = previewFormats.get(i);
+                            Log.i(TAG + "initCamera", "previewformates:" + pf);
+                        }
+
+                        // 设置拍照和预览图片大小
+                        int width = 3840;
+                        int height = 2160;
+                        parameters.setPictureSize(1920, 1080); //指定拍照图片的大小
+                        parameters.setPreviewSize(width, height); // 指定preview的大小
+                        //这两个属性 如果这两个属性设置的和真实手机的不一样时，就会报错
+
+                        // 横竖屏镜头自动调整
+                        if (getContext().getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+                            parameters.set("orientation", "portrait"); //
+                            parameters.set("rotation", 90); // 镜头角度转90度（默认摄像头是横拍）
+                            mCamera.setDisplayOrientation(90); // 在2.2以上可以使用
+                        } else// 如果是横屏
+                        {
+                            parameters.set("orientation", "landscape"); //
+                            mCamera.setDisplayOrientation(0); // 在2.2以上可以使用
+                        }
+
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+                           /* 视频流编码处理 */
+                        //添加对视频流处理函数
+
+                    } catch (Exception e) {
+                    }
                     mCamera.setParameters(parameters);
                     mCamera.stopPreview();
                     mCamera.startPreview();
                 } catch (Exception e) {
                 }
+
 
             }
         }
@@ -84,6 +145,7 @@ public class ShowCamera extends SurfaceView {
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(surfaceHolderCallback);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        setAlpha(0.8f);
     }
 
     public void stop() {
--- sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/TextureViewActivity.java
+++ sample/src/main/java/com/angcyo/sample/SurfaceViewMathDemo/TextureViewActivity.java
@@ -1,19 +1,21 @@
 package com.angcyo.sample.SurfaceViewMathDemo;
 
+import android.app.Activity;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.TextureView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.angcyo.sample.R;
 
 import java.io.IOException;
 
 @SuppressWarnings("deprecation")
-public class TextureViewActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+public class TextureViewActivity extends Activity implements TextureView.SurfaceTextureListener {
     private Camera mCamera;
     private TextureView mTextureView;
     private Button mRotate;
@@ -22,6 +24,7 @@ public class TextureViewActivity extends AppCompatActivity implements TextureVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texture_view);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
 //        mTextureView = new TextureView(this);
         mTextureView = (TextureView) findViewById(R.id.textureView);
@@ -29,28 +32,33 @@ public class TextureViewActivity extends AppCompatActivity implements TextureVie
 
         mTextureView.setSurfaceTextureListener(this);
 
+        mTextureView.setAlpha(0.5f);
 
-        mRotate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rotate += 90;
-                mTextureView.setRotation(rotate % 360);
-            }
+        mRotate.setOnClickListener(v -> {
+            rotate += 90;
+            mTextureView.setRotation(rotate % 360);
         });
     }
 
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
         mCamera = Camera.open();
+        mCamera.setDisplayOrientation(90);
+
         Camera.Parameters parameters = mCamera.getParameters();
         parameters.setPreviewSize(1920, 1080);
         parameters.setPictureSize(1920, 1080);
-        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+//        parameters.setRotation(90);
 //        parameters.setPreviewFormat();
         mCamera.setParameters(parameters);
         try {
             mCamera.setPreviewTexture(surface);
             mCamera.startPreview();
+            mCamera.autoFocus((success, camera) -> {
 
+            });
+            mCamera.cancelAutoFocus();
 //            mCamera.setDisplayOrientation(Surface.ROTATION_90);
         } catch (IOException ioe) {
             // Something bad happened
@@ -69,6 +77,11 @@ public class TextureViewActivity extends AppCompatActivity implements TextureVie
 
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
         // Invoked every time there's a new Camera preview frame
+    }
+
+    public void getBitmap(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageBitmap(mTextureView.getBitmap(imageView.getWidth(), imageView.getHeight()));
     }
 
 }
