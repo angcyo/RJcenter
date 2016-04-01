075bc942838e7e33fdf5b194686c41f1dfa5d0df
angcyo <angcyo@126.com>
2016-04-01 23:31:21 +0800

    add qq drag layout demo

--- sample/src/main/java/com/angcyo/demo/draglayout/DragLayoutActivity.java
+++ sample/src/main/java/com/angcyo/demo/draglayout/DragLayoutActivity.java
@@ -2,6 +2,7 @@ package com.angcyo.demo.draglayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,17 +22,24 @@ public class DragLayoutActivity extends Activity {
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         View view = findViewById(R.id.button);
         View view2 = findViewById(R.id.button2);
+        View view3 = findViewById(R.id.button3);
+        View view4 = findViewById(R.id.button4);
+        View view5 = findViewById(R.id.button5);
 
         DragTouchListener dragTouchListener = new DragTouchListener();
         view.setOnTouchListener(dragTouchListener);
         imageView.setOnTouchListener(dragTouchListener);
         view2.setOnTouchListener(dragTouchListener);
+        view3.setOnTouchListener(dragTouchListener);
+        view4.setOnTouchListener(dragTouchListener);
+        view5.setOnTouchListener(dragTouchListener);
     }
 
     class DragTouchListener implements View.OnTouchListener {
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+            Log.e("angcyo", "onTouch");
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 dragLayout.setDragView(v);
             }
--- sample/src/main/java/com/angcyo/demo/draglayout/RDragLayout.java
+++ sample/src/main/java/com/angcyo/demo/draglayout/RDragLayout.java
@@ -3,6 +3,9 @@ package com.angcyo.demo.draglayout;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -11,6 +14,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.rsen.util.ResUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by angcyo on 2016-03-31 23:07.
  */
@@ -18,8 +26,12 @@ public class RDragLayout extends RelativeLayout {
 
     public static final boolean DEBUG = true;
     public static final String TAG = "RDragLayout";
-    private Bitmap dragViewBitmap;//当前拖动View的Bitmap
+    //    private Bitmap dragViewBitmap;//当前拖动View的Bitmap
     private ImageView dragImageView;
+    List<Rect> gridList;
+    Paint paint;
+    private int gridNum = 6;        //横向格子数量
+
 
     public RDragLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -28,37 +40,66 @@ public class RDragLayout extends RelativeLayout {
     }
 
     private void initLayout() {
-        dragImageView = new ImageView(getContext());
-        dragImageView.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        setWillNotDraw(false);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    private ImageView getImageView(Bitmap bitmap) {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        imageView.setImageBitmap(bitmap);
+        imageView.setClickable(false);
+        imageView.setFocusable(false);
+        return imageView;
     }
 
     public void setDragView(View view) {
-        if (dragViewBitmap != null) {
-            dragViewBitmap.recycle();
-        }
-        dragViewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap dragViewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(dragViewBitmap);
         view.draw(canvas);
-        dragImageView.setImageBitmap(dragViewBitmap);
+
+        dragImageView = getImageView(dragViewBitmap);
+
+        float scale = 1.2f;
+        dragImageView.setScaleX(scale);
+        dragImageView.setScaleY(scale);
 
         addView(dragImageView);
-        dragView(view.getLeft() + 40, view.getTop() + 40);
+        dragView(view.getLeft(), view.getTop());
     }
 
     private void stopDragView(float x, float y) {
-        ImageView imageView = new ImageView(getContext());
-        imageView.setImageBitmap(dragViewBitmap);
-        imageView.setX(x);
-        imageView.setY(y);
-        addView(imageView);
+//        ImageView imageView = new ImageView(getContext());
+//        imageView.setImageBitmap(dragViewBitmap);
+//        imageView.setX(x);
+//        imageView.setY(y);
+//        addView(imageView);
+//
+////        if (dragViewBitmap != null) {
+////            dragViewBitmap.recycle();
+////            dragViewBitmap = null;
+////        }
+//
+//        dragImageView.setImageBitmap(null);
+//        removeView(dragImageView);
+        Rect rect = findRect(x, y);
+        if (rect == null) {
+            removeView(dragImageView);
+        } else {
+            dragImageView.setScaleY(1f);
+            dragImageView.setScaleX(1f);
+            dragView(rect.centerX(), rect.centerY());
+        }
+    }
 
-//        if (dragViewBitmap != null) {
-//            dragViewBitmap.recycle();
-//            dragViewBitmap = null;
-//        }
-
-        dragImageView.setImageBitmap(null);
-        removeView(dragImageView);
+    private Rect findRect(float x, float y) {
+        for (Rect rt : gridList) {
+            if (rt.contains((int) x, (int) y)) {
+                return rt;
+            }
+        }
+        return null;
     }
 
     private void startDragView(float x, float y) {
@@ -66,27 +107,25 @@ public class RDragLayout extends RelativeLayout {
     }
 
     private void dragView(float x, float y) {
-        dragImageView.setVisibility(VISIBLE);
-        dragImageView.setX(x);
-        dragImageView.setY(y);
+        int width = dragImageView.getWidth();
+        int height = dragImageView.getHeight();
+
+        dragImageView.setX(x - width / 2);
+        dragImageView.setY(y - height / 2);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
 //        e("onInterceptTouchEvent");
-        boolean intercept = false;
         if (ev.getAction() == MotionEvent.ACTION_MOVE) {
             if (dragImageView.getParent() != null) {
                 dragView(ev.getX(), ev.getY());
-                intercept = true;
             }
         } else if (ev.getAction() == MotionEvent.ACTION_UP) {
             if (dragImageView.getParent() != null) {
                 stopDragView(ev.getX(), ev.getY());
                 //判断是否可以放下
                 //TODO: 16-03-31-031
-
-                intercept = true;
             }
         } else if (ev.getAction() == MotionEvent.ACTION_DOWN) {
 //            if (dragImageView.getVisibility() == View.INVISIBLE) {
@@ -95,7 +134,54 @@ public class RDragLayout extends RelativeLayout {
 //            }
         }
 
-        return !intercept && super.onInterceptTouchEvent(ev);
+//        if (intercept) {
+//            return true;
+//        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int paddingLeft = (int) ResUtil.dpToPx(getResources(), 110);
+
+        int measuredWidth = getMeasuredWidth() - paddingLeft;
+        int gridWidth = (int) Math.ceil(measuredWidth / (float) gridNum);//每个格子的宽度
+
+        int measuredHeight = getMeasuredHeight();
+        int gridHeightNum = (int) Math.ceil(measuredHeight / (float) gridWidth);//高度的个数
+
+
+        Rect rect;
+        gridList = new ArrayList<>();
+        for (int j = 0; j < gridHeightNum; j++) {
+            //纵向第几个
+            for (int i = 0; i < gridNum; i++) {
+                //横向第几个
+                rect = new Rect(0, 0, gridWidth, gridWidth);
+                rect.offsetTo(i * gridWidth + paddingLeft, j * gridWidth);
+                gridList.add(rect);
+
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (int i = 0; i < gridList.size(); i++) {
+            int line = i / gridNum;//第几行
+            int col = i % gridNum;//第几列
+
+            if (line % 2 == 0 && col % 2 == 0 || line % 2 != 0 && col % 2 != 0) {
+                paint.setColor(Color.GRAY);
+            } else {
+                paint.setColor(Color.YELLOW);
+            }
+            canvas.drawRect(gridList.get(i), paint);
+        }
+
+        super.onDraw(canvas);
     }
 
     @Override
@@ -106,7 +192,7 @@ public class RDragLayout extends RelativeLayout {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-//        e("dispatchTouchEvent");
+        e("dispatchTouchEvent");
         return super.dispatchTouchEvent(ev);
     }
 
