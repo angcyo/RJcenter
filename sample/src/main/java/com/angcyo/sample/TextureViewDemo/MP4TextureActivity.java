15a2f7b3d4a343916c9b997a074f6e81c47950cb
angcyo <angcyo@126.com>
2016-04-06 20:36:33 +0800

    add mp4 video play view

--- sample/src/main/java/com/angcyo/sample/TextureViewDemo/MP4TextureActivity.java
+++ sample/src/main/java/com/angcyo/sample/TextureViewDemo/MP4TextureActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.angcyo.sample.R;
+import com.rsen.media.VideoTextureView;
 
 public class MP4TextureActivity extends Activity {
 
@@ -11,5 +12,15 @@ public class MP4TextureActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp4_texture);
+
+        VideoTextureView carView = (VideoTextureView) findViewById(R.id.carView);
+        VideoTextureView carView2 = (VideoTextureView) findViewById(R.id.carView2);
+        carView2.setAlpha(0.6f);
+        carView.setAlpha(0.5f);
+        carView.setPlayListener(() -> {
+            carView.setFilePath("/sdcard/robi/cycle.mp4");
+            carView.setLoopMode(true);
+            carView.startPlay();
+        });
     }
 }
