aafe5bb72db4b1019bef89f371e2bb425ca46194
angcyo <angcyo@126.com>
2016-04-07 16:35:57 +0800

    add thread demo

--- javademo/src/main/java/com/example/ThreadDemo.java
+++ javademo/src/main/java/com/example/ThreadDemo.java
@@ -9,23 +9,36 @@ public class ThreadDemo {
     static final PrintStream p = System.out;
 
     public static void main(String... args) {
-        new Thread() {
-            @Override
-            public void run() {
-                long startTime = System.currentTimeMillis();
-                long index = 0;
-                while (true) {
-                    ++index;
-                    long nowTime = System.currentTimeMillis();
-                    if ((nowTime - startTime) / 1000 >= 1) {
-                        p.println(index);
-                        index = 0;
-                        startTime = nowTime;
-//                        break;
-                    }
-                }
-//                p.println(index);
-            }
-        }.start();
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                long startTime = System.currentTimeMillis();
+//                long index = 0;
+//                while (true) {
+//                    ++index;
+//                    long nowTime = System.currentTimeMillis();
+//                    if ((nowTime - startTime) / 1000 >= 1) {
+//                        p.println(index);
+//                        index = 0;
+//                        startTime = nowTime;
+////                        break;
+//                    }
+//                }
+////                p.println(index);
+//            }
+//        }.start();
+
+        ThreadNew threadNew = new ThreadNew();
+        threadNew.start();
+        threadNew.start();
+    }
+
+
+    static class ThreadNew extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            p.println(Thread.currentThread().getId() + "-->" + System.currentTimeMillis());
+        }
     }
 }
