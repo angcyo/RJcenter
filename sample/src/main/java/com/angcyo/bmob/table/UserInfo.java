fe390fddfaf66980fa3a9501eafebb5ec94758c7
angcyo <angcyo@126.com>
2016-04-13 17:51:03 +0800

    新增用户表

---
+++ sample/src/main/java/com/angcyo/bmob/table/UserInfo.java
@@ -0,0 +1,12 @@
+package com.angcyo.bmob.table;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by robi on 2016-04-13 17:23.
+ */
+public class UserInfo extends BmobObject{
+    //名称
+    //注册码
+    //
+}
