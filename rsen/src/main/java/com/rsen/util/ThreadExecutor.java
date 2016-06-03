28185dac9fc636f0bae348d3b27e1191c9eadc91
angcyo <angcyo@126.com>
2016-06-03 10:55:05 +0800

    add  thread executor

--- rsen/src/main/java/com/rsen/util/ThreadExecutor.java
+++ rsen/src/main/java/com/rsen/util/ThreadExecutor.java
@@ -4,31 +4,39 @@ import android.os.Handler;
 import android.os.Looper;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 主线程调度器
  * Created by robi on 2016-06-02 20:47.
  */
-public class MainThreadExecutor {
-    static private MainThreadExecutor instance;
+public class ThreadExecutor {
+    static private ThreadExecutor instance;
     private final Executor mCallbackPoster;
+    private final ExecutorService mExecutorService;
     private final Handler handler = new Handler(Looper.getMainLooper());
 
-    private MainThreadExecutor() {
+    private ThreadExecutor() {
         mCallbackPoster = new Executor() {
             @Override
             public void execute(Runnable command) {
                 handler.post(command);
             }
         };
+
+        mExecutorService = Executors.newCachedThreadPool();
     }
 
-    public static MainThreadExecutor instance() {
-        return instance == null ? instance = new MainThreadExecutor() : instance;
+    public static ThreadExecutor instance() {
+        return instance == null ? instance = new ThreadExecutor() : instance;
     }
 
     public void onMain(Runnable runnable) {
         mCallbackPoster.execute(runnable);
     }
 
+    public void onThread(Runnable runnable) {
+        mExecutorService.execute(runnable);
+    }
 }
