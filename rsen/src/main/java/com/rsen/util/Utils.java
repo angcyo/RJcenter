e173131a3a7b9ee2089b580c6d16ee5006e79a8d
angcyo <angcyo@126.com>
2016-06-12 14:58:38 +0800

    add delete item into base adapter

--- rsen/src/main/java/com/rsen/util/Utils.java
+++ rsen/src/main/java/com/rsen/util/Utils.java
@@ -1,5 +1,6 @@
 package com.rsen.util;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -12,6 +13,7 @@ import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -21,6 +23,76 @@ import java.util.Random;
  */
 public class Utils {
 
+
+    private static final String[][] MIME_MapTable = {
+            //{后缀名，MIME类型}
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            {".xls", "application/vnd.ms-excel"},
+            {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+            {".prop", "text/plain"},
+            {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"},
+            {".rtf", "application/rtf"},
+            {".sh", "text/plain"},
+            {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"},
+            {".txt", "text/plain"},
+            {".wav", "audio/x-wav"},
+            {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"},
+            {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/x-zip-compressed"},
+            {"", "*/*"}
+    };
 
     /**
      * 去除字符串左右的字符
@@ -76,7 +148,6 @@ public class Utils {
         return (str == null || str.trim().length() < 1);
     }
 
-
     /**
      * Gets time.
      *
@@ -129,6 +200,8 @@ public class Utils {
         return format.format(new Date());
     }
 
+//
+
     /**
      * Gets date and time.
      *
@@ -140,8 +213,6 @@ public class Utils {
         return format.format(new Date());
     }
 
-//
-
     /**
      * @param context 上下文
      * @return 返回手机号码 tel number
@@ -151,7 +222,6 @@ public class Utils {
                 .getSystemService(Context.TELEPHONY_SERVICE);
         return tm.getLine1Number();
     }
-
 
     /**
      * Gets os version.
@@ -268,7 +338,6 @@ public class Utils {
         return code;
     }
 
-
     /**
      * 获取屏幕的宽度高度
      *
@@ -284,7 +353,6 @@ public class Utils {
         windowManager.getDefaultDisplay().getMetrics(outMetrics);
         return outMetrics;
     }
-
 
     /**
      * 返回调用行的方法 所在行号
@@ -338,7 +406,6 @@ public class Utils {
         return result.toString();
     }
 
-
     /**
      * 打开网页,调用系统应用
      *
@@ -357,5 +424,49 @@ public class Utils {
         Uri webPage = Uri.parse(url);
         Intent webIntent = new Intent(Intent.ACTION_VIEW, webPage);
         context.startActivity(webIntent);
+    }
+
+    /**
+     * 打开文件
+     *
+     * @param file
+     */
+    public static void openFile(Context context, File file) throws ActivityNotFoundException {
+
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //设置intent的Action属性
+        intent.setAction(Intent.ACTION_VIEW);
+        //获取文件file的MIME类型
+        String type = getMIMEType(file);
+        //设置intent的data和Type属性。
+        intent.setDataAndType(/*uri*/Uri.fromFile(file), type);
+        //跳转
+        context.startActivity(intent);     //这里最好try一下，有可能会报错。 //比如说你的MIME类型是打开邮箱，但是你手机里面没装邮箱客户端，就会报错。
+    }
+
+    /**
+     * 根据文件后缀名获得对应的MIME类型。
+     *
+     * @param file
+     */
+    private static String getMIMEType(File file) {
+
+        String type = "*/*";
+        String fName = file.getName();
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+    /* 获取文件的后缀名*/
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (end == "") return type;
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) { //MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？
+            if (end.equals(MIME_MapTable[i][0]))
+                type = MIME_MapTable[i][1];
+        }
+        return type;
     }
 }
