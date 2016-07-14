4e8ee85c3948e05042610547d136ed42143ac2cb
angcyo <angcyo@126.com>
2016-07-14 22:25:43 +0800

    add rx demo https address

--- sample/src/main/java/com/angcyo/sample/rx/RxAssistOperator.java
+++ sample/src/main/java/com/angcyo/sample/rx/RxAssistOperator.java
@@ -1,9 +1,25 @@
 package com.angcyo.sample.rx;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.functions.Func1;
+
 /**
  * Rx 辅助操作符
  * https://mcxiaoke.gitbooks.io/rxdocs/content/operators/Observable-Utility-Operators.html
  * Created by angcyo on 2016-07-14.
  */
+@SuppressWarnings("unchecked")
 public class RxAssistOperator {
+    public static void delayDemo() {
+        RxDemo.log(RxDemo.getMethodName() + " ----start");
+        Observable.range(1, 100).map(new Func1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                RxDemo.log(RxDemo.getMethodName() + " " + integer);
+                return integer;
+            }
+        }).delay(5, TimeUnit.SECONDS).subscribe(new RxCreateOperator.Sub());
+    }
 }
