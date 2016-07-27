34e96120902889f31866919e1cf4356a25eab7fc
angcyo <angcyo@126.com>
2016-07-27 18:25:46 +0800

    add bluetooth demo

--- sample/src/main/java/com/angcyo/bluetooth/BluetoothReceiver.java
+++ sample/src/main/java/com/angcyo/bluetooth/BluetoothReceiver.java
@@ -4,15 +4,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.lib.common.RLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 蓝牙相关广播接收
  * Created by robi on 2016-07-27 15:43.
  */
 public class BluetoothReceiver extends BroadcastReceiver {
-    //    public static Logger log = LoggerFactory.getLogger("bluetooth.receiver");
-    RLog log = RLog.getLog("bluetooth.receiver");
+    public static Logger log = LoggerFactory.getLogger("bluetooth.receiver");
+//    RLog log = RLog.getLog("bluetooth.receiver");
 
     @Override
     public void onReceive(Context context, Intent intent) {
