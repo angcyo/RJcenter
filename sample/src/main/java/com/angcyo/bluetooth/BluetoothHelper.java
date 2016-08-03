9aa9d969d7c10c4ab103a9c422f95102068c1fcd
angcyo <angcyo@126.com>
2016-08-03 17:25:30 +0800

    add support bluetooth method

--- sample/src/main/java/com/angcyo/bluetooth/BluetoothHelper.java
+++ sample/src/main/java/com/angcyo/bluetooth/BluetoothHelper.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.os.Build;
+import android.text.TextUtils;
 
 /**
  * Created by robi on 2016-07-27 11:00.
@@ -18,5 +19,15 @@ public class BluetoothHelper {
             adapter = bluetoothManager.getAdapter();
         }
         return adapter;
+    }
+
+    /**
+     * 判断是否支持蓝牙
+     */
+    public static boolean isSupportBluetooth(BluetoothAdapter adapter) {
+        if (adapter != null && !TextUtils.isEmpty(adapter.getAddress())) {
+            return true;
+        }
+        return false;
     }
 }
