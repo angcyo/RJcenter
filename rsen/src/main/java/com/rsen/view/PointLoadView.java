2f59694ec51497de16be5c331fc83dadb07ab5da
angcyo <angcyo@126.com>
2016-09-26 19:27:22 +0800

    point load view

--- rsen/src/main/java/com/rsen/view/PointLoadView.java
+++ rsen/src/main/java/com/rsen/view/PointLoadView.java
@@ -1,5 +1,6 @@
 package com.rsen.view;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -18,6 +19,31 @@ public class PointLoadView extends View {
     Paint mPaint;
     ValueAnimator mValueAnimator;
     float mCurrentY;
+    float mCurrentDegrees;
+    boolean needStop = false;//循环一周后, 需要让动画停下来
+    /**
+     * 点的数量
+     */
+    int mPointCount = 5;
+    /**
+     * 点的半径
+     */
+    float mPointRadius = 10;
+
+    /**
+     * 点之间的距离
+     */
+    float mPointSpace = 10;
+
+    /**
+     * 点之间, 动画差值角度
+     */
+    int mPointOffsetDegrees = 45;
+
+    /**
+     * 动画结束的点的索引,必须小于等于点的数量
+     */
+    int mPointEndIndex = 0;
 
     public PointLoadView(Context context) {
         super(context);
@@ -36,7 +62,29 @@ public class PointLoadView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawCircle(getMeasuredWidth() / 2, mCurrentY, 10, mPaint);
+        boolean set = true;
+        for (int i = 0; i < mPointCount; i++) {
+            final float cy = getPointYWidthDegres(mCurrentDegrees - i * mPointOffsetDegrees);
+
+            if (i < mPointEndIndex) {
+                //点的动画已经结束了
+                drawCircle(canvas, mPointRadius + i * (2 * mPointRadius + mPointSpace), getMeasuredHeight() / 2, Color.RED);
+            } else {
+                if (needStop && set && i == (mPointEndIndex + 1) && cy >= getMeasuredHeight() / 2) {
+                    drawCircle(canvas, mPointRadius + i * (2 * mPointRadius + mPointSpace), getMeasuredHeight() / 2, Color.RED);
+                    mPointEndIndex++;
+                    set = false;
+                } else {
+                    drawCircle(canvas, mPointRadius + i * (2 * mPointRadius + mPointSpace), cy, Color.GREEN);
+                }
+            }
+
+            if (mPointEndIndex == mPointCount) {
+                //所有动画结束
+                Log.i(TAG, "onDraw: 结束一周.");
+                stop();
+            }
+        }
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
     }
@@ -47,14 +95,15 @@ public class PointLoadView extends View {
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
         if (MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.AT_MOST) {
-            width = 100;
+            width = 600;
             Log.d(TAG, "onMeasure: width");
         }
         if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST) {
             height = 100;
             Log.d(TAG, "onMeasure: height");
         }
-
+//        height += mPointRadius * 2;
+//        width += mPointRadius * 2;
         setMeasuredDimension(width, height);
         mCurrentY = height / 2;
     }
@@ -103,6 +152,11 @@ public class PointLoadView extends View {
 
     //--------------------私有的方法------------------------//
 
+    private void drawCircle(Canvas canvas, float cx, float cy, int color) {
+        mPaint.setColor(color);
+        canvas.drawCircle(cx, cy, mPointRadius, mPaint);
+    }
+
     private void initView() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(Color.GREEN);
@@ -114,14 +168,35 @@ public class PointLoadView extends View {
         mValueAnimator.setInterpolator(new LinearInterpolator());
 //        mValueAnimator.setRepeatMode(ValueAnimator.REVERSE);
         mValueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        mValueAnimator.setDuration(700);
+        mValueAnimator.setDuration(2000);
         mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                float sin = (float) Math.sin(Math.toRadians((Integer) animation.getAnimatedValue()));
-                Log.d(TAG, "onAnimationUpdate: " + sin);
-                mCurrentY = getPointY(sin);
+                mCurrentDegrees = (Integer) animation.getAnimatedValue();
+                mCurrentY = getPointYWidthDegres(mCurrentDegrees);
+//                Log.d(TAG, "onAnimationUpdate: " + mCurrentY + " -- " + mCurrentDegrees);
                 postInvalidate();
+            }
+        });
+        mValueAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                needStop = true;
             }
         });
     }
@@ -129,13 +204,20 @@ public class PointLoadView extends View {
     //获取圆圈的cy值
     private float getPointY(float sin) {
         float cy;
-        float height = getMeasuredHeight();
-        if (sin > 0) {
-            cy = sin * (height / 2);
-        } else {
-            cy = height / 2 + Math.abs(sin) * (height / 2);
-        }
+        float rawHeight = getMeasuredHeight();
+        float height = rawHeight - 2 * mPointRadius;
+//        if (sin > 0) {
+//            cy = sin * (height / 2);
+//        } else {
+//            cy = height / 2 + Math.abs(sin) * (height / 2);
+//        }
+        cy = rawHeight / 2 - sin * (height / 2);
         return cy;
+    }
+
+    private float getPointYWidthDegres(float degrees) {
+        float sin = (float) Math.sin(Math.toRadians(degrees));
+        return getPointY(sin);
     }
 
     private void start() {
