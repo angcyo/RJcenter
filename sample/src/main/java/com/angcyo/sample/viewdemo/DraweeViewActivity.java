2a24d3450864b8cc1a8db5ad127b4cba23a2612c
angcyo <angcyo@126.com>
2016-09-30 14:19:22 +0800

    add drawee view demo

--- sample/src/main/java/com/angcyo/sample/viewdemo/DraweeViewActivity.java
+++ sample/src/main/java/com/angcyo/sample/viewdemo/DraweeViewActivity.java
@@ -1,29 +1,24 @@
 package com.angcyo.sample.viewdemo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 
 import com.angcyo.sample.R;
 import com.rsen.base.RBaseActivity;
 
 public class DraweeViewActivity extends RBaseActivity {
 
+    static final String url = "http://mvimg1.meitudata.com/559e50975a5039607.jpg";
+
     @Override
     protected int getContentView() {
-        return R.layout.activity_drawee_view;
+        return R.layout.content_drawee_view;
     }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        SimpleDraweeView simpleDraweeView = (SimpleDraweeView) mViewHolder.v(R.id.drawee_view);
+//        simpleDraweeView.setImageURI(Uri.parse(url));
+//
+//        simpleDraweeView.getHierarchy().setProgressBarImage(R.mipmap.progress_bar);
     }
 }
