4888f34ce6ea7b6d40f9c0224f0cd6ad6202e6eb
angcyo <angcyo@126.com>
2016-10-08 17:04:48 +0800

    add layout full screen

--- rsen/src/main/java/com/rsen/base/RBaseActivity.java
+++ rsen/src/main/java/com/rsen/base/RBaseActivity.java
@@ -215,7 +215,8 @@ public abstract class RBaseActivity extends AppCompatActivity {
                 if (enableStatusColor()) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);//全屏布局
+                        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//全屏布局
                         window.setStatusBarColor(getStateBarColor());
                     } else {
                         int statusBarHeight = (int) Math.ceil(getStatusBarHeight(this));
