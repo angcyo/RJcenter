f57c4f6046bc18c3f3e2824a4f156172a0cd5b28
angcyo <angcyo@126.com>
2016-10-10 14:19:22 +0800

    add  anim

--- rsen/src/main/java/com/rsen/util/AnimUtil.java
+++ rsen/src/main/java/com/rsen/util/AnimUtil.java
@@ -3,18 +3,24 @@ package com.rsen.util;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by angcyo on 2016-10-02 20:54.
  */
 public class AnimUtil {
     public static void startArgb(final View targetView, int startColor, int endColor) {
+        startArgb(targetView, startColor, endColor, 700);
+    }
+
+    public static void startArgb(final View targetView, int startColor, int endColor, long duration) {
         ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), startColor, endColor);
+        colorAnimator.setInterpolator(new LinearInterpolator());
         colorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                int color = (int) animation.getAnimatedValue();//之后就可以得到动画的颜色了
-                targetView.setBackgroundColor(color);//设置一下, 就可以看到效果.
+                int color = (int) animation.getAnimatedValue();//之后就可以得到动画的颜色了.
+                targetView.setBackgroundColor(color);//设置一下, 就可以看到效果..
             }
         });
         colorAnimator.setDuration(700);
