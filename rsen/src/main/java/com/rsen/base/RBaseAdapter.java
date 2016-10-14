46a4f3fe4c2b4e82f4d974c176d8fac4b31b86a9
angcyo <angcyo@126.com>
2016-10-14 15:10:43 +0800

    fix bug

--- rsen/src/main/java/com/rsen/base/RBaseAdapter.java
+++ rsen/src/main/java/com/rsen/base/RBaseAdapter.java
@@ -23,7 +23,7 @@ public abstract class RBaseAdapter<T> extends RecyclerView.Adapter<RBaseViewHold
     }
 
     public RBaseAdapter(Context context, List<T> datas) {
-        this.mAllDatas = datas;
+        this.mAllDatas = datas == null ? new ArrayList<T>() : datas;
         this.mContext = context;
     }
 
