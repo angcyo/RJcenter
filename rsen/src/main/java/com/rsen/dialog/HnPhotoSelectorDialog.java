1e77d3ae4a8117cb878df954705a1cd31f724e59
angcyo <angcyo@126.com>
2016-10-18 12:07:05 +0800

    添加 拍照/选择图片对话框

---
+++ rsen/src/main/java/com/rsen/dialog/HnPhotoSelectorDialog.java
@@ -0,0 +1,110 @@
+package com.rsen.dialog;
+
+import android.net.Uri;
+import android.view.View;
+
+import com.angcyo.rsen.R;
+import com.rsen.base.RBaseDialogFragment;
+import com.rsen.util.BitmapHelper;
+import com.rsen.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 选择图片的对话框
+ * <p>
+ * Created by angcyo on 2016-10-10.
+ */
+
+public class HnPhotoSelectorDialog extends RBaseDialogFragment {
+
+
+    public static final int CODE_CAMERA = 101;
+    public static final int CODE_PHOTO = 102;
+
+    protected Uri mCameraImageUri;
+
+    public HnPhotoSelectorDialog() {
+    }
+
+    public static String createPhotoPath() {
+        String path = FileUtil.getPath("ZhenNn/Photo/");
+        return path + UUID.randomUUID();
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.dialog_selector_photo_layout;
+    }
+
+    @Override
+    protected void initViewEvent() {
+        mViewHolder.v(R.id.selector_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapHelper.getPhotoFromPhotos(mBaseActivity, CODE_PHOTO);
+                dismiss();
+            }
+        });
+        mViewHolder.v(R.id.take_photo_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(createPhotoPath());
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mCameraImageUri = Uri.fromFile(file);
+                BitmapHelper.getPhotoFromCamera(mBaseActivity, CODE_CAMERA, mCameraImageUri);
+                dismiss();
+            }
+        });
+        mViewHolder.v(R.id.cancel_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    public Uri getCameraImageUri() {
+        return mCameraImageUri;
+    }
+
+    public String getImagePath() {
+        if (mCameraImageUri == null) {
+            return "";
+        }
+        return mCameraImageUri.getPath();
+    }
+
+    public String getImagePath(Uri uri) {
+        return BitmapHelper.getPath(mBaseActivity, uri);
+    }
+
+//    private Uri mCropImageUri;
+//    private Uri mImageUri;
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (resultCode == RESULT_OK) {
+//            if (requestCode == CODE_PHOTO) {
+//                Uri data1 = data.getData();
+//                mCropImageUri = Uri.fromFile(new File(createPhotoPath()));
+//                BitmapHelper.cropImageByUri(this, CODE_CROP,
+//                        Uri.fromFile(new File(BitmapHelper.getPath(this, data1))), mCropImageUri,
+//                        200, 200);
+//            } else if (requestCode == CODE_CAMERA) {
+//                mImageUri = mPhotoSelectorDialog.getCameraImageUri();
+//                mCropImageUri = mImageUri;
+//                BitmapHelper.cropImageByUri(this, CODE_CROP, mImageUri, mCropImageUri, 200, 200);
+//            } else if (requestCode == CODE_CROP) {
+//                mUploadAdapter.addLastItemSafe(mCropImageUri.getPath());
+//            }
+//        }
+//    }
+}
