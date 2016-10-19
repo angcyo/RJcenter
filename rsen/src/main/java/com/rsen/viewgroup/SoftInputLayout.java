a83ed1913c9991412d361e7c97acdeda46433009
angcyo <angcyo@126.com>
2016-10-19 14:30:30 +0800

    add soft input layout

--- rsen/src/main/java/com/rsen/viewgroup/SoftInputLayout.java
+++ rsen/src/main/java/com/rsen/viewgroup/SoftInputLayout.java
@@ -3,6 +3,7 @@ package com.rsen.viewgroup;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
@@ -69,17 +70,24 @@ public class SoftInputLayout extends LinearLayout {
         }
         mContentLayout = getChildAt(0);
         mEmojiLayout = getChildAt(1);
+        mContentLayout.setVisibility(VISIBLE);
+        mEmojiLayout.setVisibility(GONE);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        //拿到第一次的高度, 一般这个时候都是布局的真是高度.之后resize会改变这个高度
         if (oldh == 0) {
             mRawLayoutHeight = h;
         }
         if (mRawLayoutHeight == h) {
             isSoftInputShow = false;
         } else {
+            //键盘弹出的时候,高度会变化, 需要隐藏表情布局
+            if (mCurrentContentHeight != h) {
+                fixContentLayoutHeight(mCurrentContentHeight, h);
+            }
             mCurrentContentHeight = h;
             isSoftInputShow = true;
             hideEmojiLayout();
@@ -90,31 +98,79 @@ public class SoftInputLayout extends LinearLayout {
 //        Log.i(TAG, "onSizeChanged: w:" + w + " h:" + h + " oldw:" + oldw + " oldh:" + oldh);
     }
 
+    /**
+     * 修改第一次打开表情之前, 键盘没有弹出之前的BUG.
+     */
+    private void fixContentLayoutHeight(final int oldH, final int newH) {
+        postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LayoutParams params = (LayoutParams) mContentLayout.getLayoutParams();
+                params.height = newH;
+                params.weight = 0;
+                requestLayout();
+                unlockContentLayoutHeight();
+            }
+        }, 100);
+    }
+
+    /**
+     * 此方法的作用就是在键盘弹出后,然后按下了键盘上的关闭键盘按钮, 布局能自适应.
+     */
+    private void unlockContentLayoutHeight() {
+        postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LayoutParams params = (LayoutParams) mContentLayout.getLayoutParams();
+                params.height = 0;
+                params.weight = 1;
+                requestLayout();
+            }
+        }, 300);
+    }
+
+    /**
+     * 强制内容和表情布局的高度,这样在键盘弹出的时候,就不会导致布局跳动了
+     */
     public void showEmojiLayout() {
         hideSoftInput(getContext(), this);
-        LayoutParams params = (LayoutParams) mContentLayout.getLayoutParams();
-        params.height = mCurrentContentHeight;
-        params.weight = 0;
+        int keyboardHeight;
+        if (mCurrentContentHeight == 0) {
+            keyboardHeight = getDefaultEmojiHeight();
+            mCurrentContentHeight = mRawLayoutHeight - keyboardHeight;
+        } else {
+            keyboardHeight = mRawLayoutHeight - mCurrentContentHeight;
+        }
 
-        LayoutParams params2 = (LayoutParams) mEmojiLayout.getLayoutParams();
-//        params2.height = mCurrentContentHeight;
-        params2.weight = 1;
+        LayoutParams contentParams = (LayoutParams) mContentLayout.getLayoutParams();
+        contentParams.height = mCurrentContentHeight;
+        contentParams.weight = 0;
+        LayoutParams emojiParams = (LayoutParams) mEmojiLayout.getLayoutParams();
+        emojiParams.height = keyboardHeight;
+        emojiParams.weight = 0;
         mEmojiLayout.setVisibility(VISIBLE);
+        requestLayout();
         isEmojiLayoutShow = true;
     }
 
+    /**
+     * 恢复成默认就行了
+     */
     public void hideEmojiLayout() {
         LayoutParams params = (LayoutParams) mContentLayout.getLayoutParams();
         params.height = 0;
         params.weight = 1;
-
         LayoutParams params2 = (LayoutParams) mEmojiLayout.getLayoutParams();
         params2.height = 0;
         params2.weight = 0;
         mEmojiLayout.setVisibility(GONE);
         isEmojiLayoutShow = false;
+        requestLayout();
     }
 
+    /**
+     * 当表情显示的时候, 按下返回键,优先隐藏表情, 然后再finish activity
+     */
     public boolean handleBack() {
         if (isEmojiLayoutShow) {
             hideEmojiLayout();
@@ -123,12 +179,12 @@ public class SoftInputLayout extends LinearLayout {
         return false;
     }
 
-    public boolean isEmojiLayoutShow() {
-        return isEmojiLayoutShow;
-    }
-
     public boolean isSoftInputShow() {
         return isSoftInputShow;
+    }
+
+    public boolean isEmojiLayoutShow() {
+        return isEmojiLayoutShow;
     }
 
     public void setOnSoftInputChangeListener(OnSoftInputChangeListener onSoftInputChangeListener) {
@@ -137,6 +193,13 @@ public class SoftInputLayout extends LinearLayout {
 
     private void logWH() {
         Log.i(TAG, "logWH: W:" + getMeasuredWidth() + " H:" + getMeasuredHeight());
+    }
+
+    /**
+     * 默认的表情布局高度
+     */
+    private int getDefaultEmojiHeight() {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, getResources().getDisplayMetrics());
     }
 
     public interface OnSoftInputChangeListener {
