b7fd9561db23844c4a42f4c0a35d1cfd7f735138
angcyo <angcyo@126.com>
2016-11-16 10:44:51 +0800

    add math demo

--- javademo/src/main/java/com/example/MathJavaDemo.java
+++ javademo/src/main/java/com/example/MathJavaDemo.java
@@ -9,13 +9,17 @@ public class MathJavaDemo {
     static final PrintStream p = System.out;
 
     public static void main(String... args) {
-        final double atan1 = Math.atan2(1, 1);
-        final double atan2 = Math.atan2(1.5, 1);
-        final double atan3 = Math.atan2(-0.5, 0);
-        p.println(atan2);
-        p.println(Math.toDegrees(atan1));
-        p.println(Math.toDegrees(atan2));
-        p.println(Math.toDegrees(atan3));
+//        final double atan1 = Math.atan2(1, 1);
+//        final double atan2 = Math.atan2(1.5, 1);
+//        final double atan3 = Math.atan2(-0.5, 0);
+//        p.println(atan2);
+//        p.println(Math.toDegrees(atan1));
+//        p.println(Math.toDegrees(atan2));
+//        p.println(Math.toDegrees(atan3));
+
+        for (int i = 0; i <= 60; i++) {
+            p.println(i % 9);
+        }
 
 //        test1();
 //        p.println(Math.ceil(3.1));
