3b18e923752601d737d2d90ae784dc096976843e
angcyo <angcyo@126.com>
2016-11-22 12:51:28 +0800

    add base 64 java demo

---
+++ javademo/src/main/java/com/example/Base64Demo.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.io.PrintStream;
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2016/11/22 12:49
+ * 修改人员：Robi
+ * 修改时间：2016/11/22 12:49
+ * 修改备注：
+ * Version: 1.0.0
+ */
+public class Base64Demo {
+    static final PrintStream p = System.out;
+
+    public static void main(String... args) {
+//        p.println(Base64.Encoder.);
+    }
+}
