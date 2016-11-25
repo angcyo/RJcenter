52fca278b9c1d50536e83baec9253bc5b4069d3c
angcyo <angcyo@126.com>
2016-11-25 10:41:33 +0800

    add pop list

--- rsen/src/main/java/com/rsen/util/PopList.java
+++ rsen/src/main/java/com/rsen/util/PopList.java
@@ -23,22 +23,39 @@ import java.util.ArrayList;
 public class PopList {
 
     private static ListPopupWindow popList;
+    private static ArrayList<String> allDatas = new ArrayList<>();
+    private static ArrayAdapter<String> sArrayAdapter;
 
     public static void show(View view, ArrayList<String> datas, AdapterView.OnItemClickListener clickListener) {
-        Context context = view.getContext();
-        popList = new ListPopupWindow(context);
-        popList.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, datas));
-        popList.setWidth(LayoutParams.WRAP_CONTENT);
-        popList.setHeight(LayoutParams.WRAP_CONTENT);
-        popList.setAnchorView(view);//设置ListPopupWindow的锚点，即关联PopupWindow的显示位置和这个锚点
-        popList.setModal(true);//设置是否是模式
-        popList.setOnItemClickListener(clickListener);
-        popList.show();
+        allDatas.clear();
+        allDatas.addAll(datas);
+        if (allDatas.size() == 0) {
+            hide();
+            return;
+        }
+        if (popList == null) {
+            Context context = view.getContext();
+            popList = new ListPopupWindow(context);
+            sArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, allDatas);
+            popList.setAdapter(sArrayAdapter);
+            popList.setWidth(LayoutParams.WRAP_CONTENT);
+            popList.setHeight(LayoutParams.WRAP_CONTENT);
+            popList.setAnchorView(view);//设置ListPopupWindow的锚点，即关联PopupWindow的显示位置和这个锚点
+            //popList.setModal(true);//设置是否是模式, 设置此模式会拦截焦点和输入事件
+            popList.setOnItemClickListener(clickListener);
+            popList.show();
+        } else {
+            sArrayAdapter.notifyDataSetChanged();
+            if (!popList.isShowing()) {
+                popList.show();
+            }
+        }
     }
 
     public static void hide() {
         if (popList != null) {
             popList.dismiss();
+            allDatas.clear();
             popList = null;
         }
     }
