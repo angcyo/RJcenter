b3a3427978a3a1cc1c8a7d92081261ba50bcee60
angcyo <angcyo@126.com>
2016-12-04 22:08:38 +0800

    add tablayout

---
+++ rsen/src/main/java/com/rsen/github/tablayout/listener/CustomTabEntity.java
@@ -0,0 +1,13 @@
+package com.rsen.github.tablayout.listener;
+
+import android.support.annotation.DrawableRes;
+
+public interface CustomTabEntity {
+    String getTabTitle();
+
+    @DrawableRes
+    int getTabSelectedIcon();
+
+    @DrawableRes
+    int getTabUnselectedIcon();
+}
---
+++ rsen/src/main/java/com/rsen/github/tablayout/listener/OnTabSelectListener.java
@@ -0,0 +1,6 @@
+package com.rsen.github.tablayout.listener;
+
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
