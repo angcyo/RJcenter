3d561101f0aa7afd58efcfef7ecf68fe92c4411b
angcyo <angcyo@126.com>
2017-02-16 16:55:08 +0800

    java demo

--- javademo/src/main/java/com/example/TimeDemo.java
+++ javademo/src/main/java/com/example/TimeDemo.java
@@ -2,7 +2,9 @@ package com.example;
 
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -24,13 +26,43 @@ public class TimeDemo {
 //        p.println(1477622968 / 1000);
 //        p.println(System.currentTimeMillis());
 //        p.println(1477622968);
-        p.println(4 % 3);
-        p.println(4 % 3 + 1);
-        p.println(4 % (3 + 1));
 
-        p.println(4 / 3);
-        p.println(4 / 3 + 1);
-        p.println(4 / (3 + 1));
+//        p.println(4 % 3);
+//        p.println(4 % 3 + 1);
+//        p.println(4 % (3 + 1));
+//        p.println("--------------");
+//        p.println(Math.ceil(3f / 4));
+//        p.println(Math.ceil(4f / 4));
+//        p.println(Math.ceil(5f / 4));
+//        p.println(Math.ceil(4f / 4 + 1));
+//        p.println(Math.ceil(4f / (4 + 1)));
+
+        //main2("aaa", "bbb", "ccc");
+
+        int[] origin = new int[]{1, 1, 2, 3, 1, 4, 8, 2, 7, 5, 4, 10, 6, 5, 9};
+        List<Integer> originList = new ArrayList<>();
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int n : origin) {
+            if (originList.contains(n)) {
+                if (resultList.contains(n)) {
+                    resultList.add(n);
+                } else {
+                    resultList.add(n);
+                    resultList.add(n);
+                }
+            } else {
+                originList.add(n);
+            }
+        }
+        for (Integer n : resultList) {
+            p.print(" " + n);
+        }
+        p.println();
+    }
+
+    public static void main2(String... args) {
+        p.println(args);
     }
 
     public static String getSimpleDate(long time) {
